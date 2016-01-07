db8111315bbfec6ce44c7a9692eac4b67a0fd16d
Roman Salin <rsalin@mirantis.com>
2016-01-08 00:39:12 +0300

    Fix test

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -8,8 +8,7 @@ from lp_jira_sync import sync
 class TestSync(unittest.TestCase):
 
     def setUp(self):
-        self.lp_client = sync.LpClient()
-        self.jira_client = sync.JiraClient()
+        pass
 
     def test_config(self):
         self.assertTrue(sync.config.has_section('JIRA'))
